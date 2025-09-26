346cc432cec7df8177ea06c903814f21c37a25ee
Jorge Nelson <157376133+JorgeNelsonJr@users.noreply.github.com>
2025-09-26 16:56:45 -0300

    Delete streaming directory

--- streaming/src/main/java/com/streaming/streaming/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.streaming.streaming.repository;
-
-import com.streaming.streaming.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
